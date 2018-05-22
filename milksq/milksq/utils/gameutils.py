18e4a081a37092054ade5698d842b3f1aa712bc7
Jake Goldman <jgoldman@stuy.edu>
2018-05-22 13:03:12 -0400

    finished target assignment

--- milksq/milksq/utils/gameutils.py
+++ milksq/milksq/utils/gameutils.py
@@ -1,18 +1,40 @@
 from random import randint
 
 '''
-    players: a list of player ids for a particular game
-    returns: a dictionary denoting each player's target
+    This is a helper method for generating targets at the start of a game.
+        args:
+            players: a list of player ids for a particular game (should be ints, probably won't matter)
+        returns: a dictionary denoting each player's target
 '''
-def getTargets( players ):
+def get_targets( players ):
     targetDict = dict()
-    numPlayers = len(players)
 
-    current = 0
-    next = 1
+    # value of the first player for whom we're assigning a target
+    first = players.pop( randint(0, len(players) - 1) )
 
-    for i in range(numPlayers):
-        # get the index of the next target
-        next = randint( 0, len(players) )
+    # set the current player equal to the first player to start
+    current = first
+
+    print 'BEFORE LOOP:\nfirst:%d\ncurrent:%d\nremaining players:%s\n' % (first, current, players)
+    while len(players) != 0:
+        # get the next player (target for the previous player)
+        next = players.pop( randint( 0, len(players) - 1) )
+
         # set the current player's target
-        targetDict[current] = players[next]
+        targetDict[current] = next
+
+        # set the current player to be equal to the previous target
+        current = next
+        print 'LOOP PASS FINISHED:\ncurrent:%d\nremaining players:%s\n' % (current, players)
+
+    # set the last player's target to the first player
+    targetDict[current] = first
+
+    return targetDict
+
+
+# TESTING STUFF
+if __name__ == '__main__':
+    print 'TESTING get_targets( players ):'
+    players = [1, 2, 3, 4, 5]
+    print getTargets(players)
